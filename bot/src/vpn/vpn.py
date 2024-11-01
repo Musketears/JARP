54f0af53549f95fc95f74b37d976ed4aa21aa6e9
Ryan <ryanstack10@gmail.com>
2024-11-01 06:00:24 +0000

    add reconnect after vpn swap

--- bot/src/vpn/vpn.py
+++ bot/src/vpn/vpn.py
@@ -11,6 +11,7 @@ class VpnCommand(commands.Cog):
     @commands.command(name='vpn', help='Connects the vpn to a new region')
     async def clear(self, ctx):
         result = subprocess.run(["nordvpn", "connect"], capture_output=True, text=True)
+        await self.bot.connect(reconnect=True)
         await ctx.send(result.stdout if result.stdout else result.stderr)
 
 async def setup(bot: commands.Bot):
